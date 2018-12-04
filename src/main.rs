18b2e8ff869b5fc6333754ff40d425f169b1efa8
Matt Long <matthewhl@gmail.com>
2018-12-03 19:07:14 -0500

    problem 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::{read_to_string};
+
 fn main() {
     println!("Hello, world!");
+    let mut buffer = String::new();
+    let contents = match read_to_string("./problem1.txt") {
+        Ok(string_contents) => string_contents,
+        Err(e) => panic!("whajsdkdhaks")
+    };
+
+    println!("contents {:?}", contents);
+    let mut freq: isize = 0;
+    for line in contents.lines() {
+        let num = match line.parse::<isize>() {
+            Ok(i) => i,
+            Err(e) => panic!("NUM NOT PARSED")
+        };
+        freq += num;
+        println!("FREQ IS: {}", freq);
+    }
 }
